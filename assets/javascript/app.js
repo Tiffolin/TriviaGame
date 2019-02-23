be6b989522764f1dd0f84e19d50b951a6597efaf
Tiffo <szuchinlin3@gmail.com>
2019-02-22 19:12:48 -0500

    trying to add the unanswered section, but it isn't coming along.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -45,44 +45,57 @@ $(".start").click(function(){
 //for each question, if selected ansSelected == correctAns, then ++ to right
 //else ++ to wrong
 
+//create an empty array
+//when the user selects a btn, it will push the corresponding value into the empty array
 var userSelected =[];
 
 $(".answer").click(function(){
     var ansSelected = $(this).attr("value");
     // console.log(ansSelected);
     userSelected.push(ansSelected);
-    console.log(userSelected);
+    // console.log(userSelected);
     matchAns();
+    // emptAns();
 });
 
+//the correct answers
 var correctAns = [4, 7, 11, 13, 17, 24, 25];
 
- function matchAns(){
-
-    var unanswered = 0;
+//function to determine whether the user selected the correct answer
+function matchAns(){
     var right = 0;
     var wrong = 0;
 
-    for(i=0; i<userSelected.length; i++){
+    for(i=0; i<userSelected.length; i++){    
 
-    
     if (userSelected[i] == correctAns[i]){
-    console.log ("true");
+    // console.log ("true");
     right++;
     $("#rightNum").html(right);
     }
 
     else {
-    console.log ("false");
+    // console.log ("false");
     wrong++;
     $("#wrongNum").html(wrong);
     }
-}
+
+
+}};
+
+// var unanswered = 0;
+
+// function emptAns(){
+//     if(!$("input[name='ans1']" || "input[name='ans2']"|| "input[name='ans3']" || "input[name='ans4']"||"input[name='ans5']" || "input[name='ans6']"|| "input[name='ans7']" : "checked").val()) {
+//     console.log("unanswered");
+//     unanswered++;
+//     $("#unanswered").html(unanswered);
+//     }
     
-};
-
-
-
+//     else{
+//     matchAns();
+//     }
+// }
 
 
 //when submit button is clicked, the main body disappears, and the scoreboard appears
